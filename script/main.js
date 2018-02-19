e02bb3b5df39bd76e034782f1121e67b5d7c4aaf
basselhajjo <basselhajjo@gmail.com>
2018-02-19 03:53:18 +0100

    give tags and render all movies

---
+++ script/main.js
@@ -0,0 +1,63 @@
+const moviesURL = 'https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json';
+const loadMovies = document.querySelector('#load');
+const moviesList = document.querySelector('ul');
+
+function fetchJSONDataNew(url) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', () => {
+            const jsonData = JSON.parse(xhr.responseText);
+            resolve(jsonData);
+        });
+
+        xhr.open('get', url);
+        xhr.send();
+    });
+
+
+
+    return promise;
+}
+
+fetchJSONDataNew(moviesURL).then(movies =>{
+    
+    /*Give each movie a tag: Excellent (>=8.5), Very Good (>=8), Good (<8) based on the ratings.*/
+    
+    movies.map(movie =>{
+       if(movie.rating >= 8.5){
+           movie['tag'] = 'Excellent';
+       } else if (movie.rating >= 8 && movie.rating < 8.5){
+           movie['tag'] = 'Very Good';
+       } else {
+           movie['tag'] = 'Good'
+       }
+    });
+    console.log(movies);
+    
+    /*Render all the movies as a list*/
+    
+    loadMovies.addEventListener('click',function(){
+        moviesList.innerHTML = "";
+        movies.map(movie =>{
+            const allMovies = document.createElement('li');
+            moviesList.appendChild(allMovies);
+            allMovies.innerHTML = movie.title;
+        });
+    });
+    
+    /*Add a input field, and a button to perform search. Use .filter method on arrays to filter on the titles.*/
+    
+    
+    
+    /*Add 4 radio buttons for each tag + All tag (All, Excellent, Very Good, Good) and filter the movies based on the tag selected.*/
+    
+    
+    
+});
+
+
+
+
+
+
